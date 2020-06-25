66f38fcd5984d923c5023950a44f89497f8cade3
antonkudrin <atlet-2@mail.ru>
2020-06-25 21:48:43 +0500

    Surfing club start

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).on("ready", function () {
+    $(".regular").slick({
+   
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+
+      responsive: [
+        {
+          breakpoint: 2570,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+           
+          },
+        },
+
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  });
